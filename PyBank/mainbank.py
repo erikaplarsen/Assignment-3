f323c53d0b16f42517ebdd73fadc47af5720da12
Erika Larsen <dennisblarsen@Erikas-MacBook-Air.local>
2019-03-02 18:58:40 -0600

    Saturday Changes

--- PyBank/mainbank.py
+++ PyBank/mainbank.py
@@ -2,22 +2,34 @@
 
 import os
 import csv
+import numpy
 
-BankCsvPath = os.path.join("budget_data.csv")
+BankCsvPath = os.path.join("desktop", "Assignment-3", "PyBank", "budget_data.csv")
 
-
-date = []
-ProfitLoss = []
+months = 0
+ProfitLoss = 0
+previous = 0 
+change = 0
+ChangeList = []
+AverageChange = 0
 
 with open(BankCsvPath, newline="") as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=",")
-    csv_header = next(csvfile)
+  csv_reader = csv.reader(csvfile, delimiter=",")
+  csv_header = next(csvfile)
 
-    for row in csv_reader:
-       months.append(row[0])
-       ''' gainloss = sum(gainloss)
-        print(row)
-      for col in gainloss
-            total += int(col)
-        print total'''
+  for row in csv_reader:
+  
+    ProfitLoss += int(row[1])
+    months += 1
 
+    change = float(row[1]) - previous
+    previous = float(int(row[1]))
+    ChangeList = ChangeList + [change]
+    
+  
+  
+  AverageChange = (sum(ChangeList))/(len(ChangeList))
+
+
+print (f"Total revenue over {months} months = {ProfitLoss}")
+print (f"Average change = {AverageChange}")
